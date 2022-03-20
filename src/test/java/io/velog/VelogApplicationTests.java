3b771b9ea3d95c5a17f299d7eaab1d0d00b306e4
monkeyDugi <lbd4946@gmail.com>
2022-03-20 09:45:57 +0900

    first commit

---
+++ src/test/java/io/velog/VelogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.velog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
